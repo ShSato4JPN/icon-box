e3998e7b72ffb80c9aa1d9f76225c041eab77fac
ShSato4JPN <shsato4.private.1221@gmail.com>
2025-08-04 22:42:50 +0900

    feat: add @t3-oss/env-nextjs

---
+++ src/env.ts
@@ -0,0 +1,17 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    AUTH_SECRET: z.string(),
+    AUTH_URL: z.url(),
+    AUTH_GITHUB_ID: z.string(),
+    AUTH_GITHUB_SECRET: z.string(),
+  },
+  runtimeEnv: {
+    AUTH_SECRET: process.env.AUTH_SECRET,
+    AUTH_URL: process.env.AUTH_URL,
+    AUTH_GITHUB_ID: process.env.AUTH_GITHUB_ID,
+    AUTH_GITHUB_SECRET: process.env.AUTH_GITHUB_SECRET,
+  },
+});
